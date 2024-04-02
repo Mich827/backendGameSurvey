11d7bc22c0ab7a915cf1ca02de17885c340d9ae1
Mich827 <100875018+Mich827@users.noreply.github.com>
2024-04-02 12:00:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: 'https://frontend-game-survey.vercel.app' }));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
